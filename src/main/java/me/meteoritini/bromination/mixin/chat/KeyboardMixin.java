6b12b75ad7fc50c16f8a9c0f7e1a04fb61033fd7
Taubsie <tobias@joerg-hartmann.de>
2026-01-16 12:34:51 +0100

    Updated the mod to 1.21.10

--- src/main/java/me/meteoritini/bromination/mixin/chat/KeyboardMixin.java
+++ src/main/java/me/meteoritini/bromination/mixin/chat/KeyboardMixin.java
@@ -2,6 +2,7 @@ package me.meteoritini.bromination.mixin.chat;
 
 import me.meteoritini.bromination.ChatOptions;
 import net.minecraft.client.Keyboard;
+import net.minecraft.client.input.KeyInput;
 import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(Keyboard.class)
 public class KeyboardMixin {
     @Inject(method = "onKey", at = @At("HEAD"))
-    private void onKey(long window, int key, int scancode, int action, int modifiers, CallbackInfo ci) {
-        if(key == GLFW.GLFW_KEY_LEFT_CONTROL || key == GLFW.GLFW_KEY_RIGHT_CONTROL) ChatOptions.ctrlPressed = action >= 1;
+    private void onKey(long window, int action, KeyInput input, CallbackInfo ci) {
+        if(input.key() == GLFW.GLFW_KEY_LEFT_CONTROL || input.key() == GLFW.GLFW_KEY_RIGHT_CONTROL) ChatOptions.ctrlPressed = action >= 1;
     }
 }
